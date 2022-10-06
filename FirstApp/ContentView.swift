ca646d9f18455d2d41892a9f98aba02e07f18741
Valentina Ronchi <valentina2.ronchi@live.uwe.ac.uk>
2022-10-06 17:56:54 +0100

    first commit

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -9,16 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack() {
+            HStack(){
+                Image("DrakeMeme1")
+                    .resizable()
+                    .scaledToFit()
+                Image("TextMeme1")
+                    .resizable()
+                    .scaledToFit()
+            }
+            HStack(){
+                Image("DrakeMeme2")
+                    .resizable()
+                    .scaledToFit()
+                Image("TextMeme2")
+                    .resizable()
+                    .scaledToFit()
+            }
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct ContentView_Previews2: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
